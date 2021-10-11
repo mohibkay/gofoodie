b42cf9e13fb2bfcc2fd3d84f7f4db418e122a346
Mohib Khan <mohibk.in@gmail.com>
2021-10-11 21:59:31 +0530

    feat: add toppings actions, reducer, & store

--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,7 @@ import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import categoriesReducer from "../reducers/categoriesReducer";
 import productsReducer from "../reducers/productsReducer";
+import toppingsReducer from "../reducers/toppingsReducer";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -9,6 +10,7 @@ const store = createStore(
   combineReducers({
     categories: categoriesReducer,
     products: productsReducer,
+    toppings: toppingsReducer,
   }),
   composeEnhancers(applyMiddleware(thunk))
 );
